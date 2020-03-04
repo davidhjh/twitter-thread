1d97749ed20da6e342f9c006231ef75c18429d2f
David Huang <davidhuang@davids-mbp.war.can.ibm.com>
2020-03-04 12:11:57 -0500

    Created multiple search csv files

--- gettweet.py
+++ gettweet.py
@@ -1,23 +1,35 @@
 import csv
 import tweepy
 import numpy
+import apikeys
 
+
+def buildTestSet(api, data, new):
+    if (new ==1):
+        search_keyword = data
+        try:
+            tweets_fetched = api.GetSearch(search_keyword, count=1000)
+            print("Fetched " + str(len(tweets_fetched)) + " tweets for the term " + search_keyword)
+            return [{"text":status.text, "label":None} for status in tweets_fetched]
+        except:
+            print("Unfortunately, something went wrong..")
+            return None
 
 
 if __name__ == "__main__":
 
     # get credentials at developer.twitter.com
-    auth = tweepy.OAuthHandler('SyABIQE3iEugeygKIGuLWWmSD', 'ozotNuRfw6EfMzxq50ntIE3vK7mi2qUZwB08gOXUotMCgWfg60')
-    auth.set_access_token('1136359757198110727-TkXiorAxiVezilL7gfrKloMFuffwAP', 'Rsl201YVdK1dKcyh2PR75kV1hWa2GAgM6Go5aKHtGXiVJ')
+    auth = tweepy.OAuthHandler(apikeys.CONSUMER_KEY, apikeys.CONSUMER_SECRET)
+    auth.set_access_token(apikeys.ACCESS_TOKEN, apikeys.ACCESS_TOKEN_SECRET)
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
     # update these for whatever tweet you want to process replies to
     name = 'elonmusk'
-    tweet_id = '1234791532391260160'
+    tweet_id = '1231330415639552001'
 
     replies=[]
-    for tweet in tweepy.Cursor(api.search,q='to:'+name, since_id=tweet_id, tweet_mode='extended', timeout=1805).items(100):
+    for tweet in tweepy.Cursor(api.search,q='to:'+name, since_id=tweet_id, tweet_mode='extended', timeout=1805).items(10000):
         if hasattr(tweet, 'in_reply_to_status_id_str'):
             if (tweet.in_reply_to_status_id_str==tweet_id):
                 # replies.append(tweet)
@@ -26,10 +38,8 @@ if __name__ == "__main__":
     # sorted_retweets = sorted(replies, key = lambda x: x[1], reverse=True)
     sorted_favorites = sorted(replies, key = lambda x: x[2], reverse=True)
 
-    # dt=numpy.dtype('str,int,int,str')
-    # print(numpy.array(sorted_favorites,dtype=dt))
-
-    f = open('replies.csv','w')
+    f = open('replies_elon_musk2.csv','w')
     csv_out=csv.writer(f)
     for row in sorted_favorites:
         csv_out.writerow(row)
+
---
+++ gettweetsearch.py
@@ -0,0 +1,37 @@
+import csv
+import tweepy
+import numpy
+import apikeys
+
+
+def tweetBySearch(api, search_keyword):
+
+    replies = []
+    for tweet in tweepy.Cursor(api.search,q=search_keyword, tweet_mode='extended', timeout=1805).items(500):
+        if hasattr(tweet, 'full_text'):
+            if (search_keyword in tweet.full_text):
+                # replies.append(tweet)
+                replies += [(tweet.user.name, int(tweet.retweet_count), int(tweet.favorite_count), tweet.full_text)]
+
+    return replies
+
+
+if __name__ == "__main__":
+
+    # get credentials at developer.twitter.com
+    auth = tweepy.OAuthHandler(apikeys.JENNY_CONSUMER_KEY, apikeys.JENNY_CONSUMER_SECRET)
+    auth.set_access_token(apikeys.JENNY_ACCESS_TOKEN, apikeys.JENNY_ACCESS_TOKEN_SECRET)
+
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    # change this value to search for different keyword(s)
+    keyword = "Artificial Intelligence"
+    replies = tweetBySearch(api, keyword)
+
+    # sorted_retweets = sorted(replies, key = lambda x: x[1], reverse=True)
+    sorted_favorites = sorted(replies, key = lambda x: x[2], reverse=True)
+
+    f = open('tweetsearch.csv','w')
+    csv_out=csv.writer(f)
+    for row in sorted_favorites:
+        csv_out.writerow(row)
